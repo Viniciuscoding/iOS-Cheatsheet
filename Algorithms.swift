f20fc1d3c90e3c999d2f84460b531be728787c6b
Viniciuscoding <vsg232@nyu.edu>
2020-04-15 20:13:30 -0400

    Update Algorithms.swift

--- Algorithms.swift
+++ Algorithms.swift
@@ -47,6 +47,50 @@ func ibinarysearch(_ array: [Int],_ value: Int) -> Int? {
 }
 
 
+// FIBONACCI NUMBER
 
+// Recursive Fibonacci Number
+func rFibNum(_ position: Int) -> Int {
+    if position == 0 || position == 1 {
+        return position
+    } else {
+        let fib = getFib(position - 1) + getFib(position - 2)
+        return fib
+    }
+}
 
+// This is how the recursion takes place
+// position = 4
+// getFib(4) = ?
+// fib = gF(3) + gF(2)
+// fib = gF(2) + gF(1) + gF(1) + gF(0)
+// fib = gF(1) + gF(0) + gF(1) + gF(1) + gF(0)
+// fib =   1   +   0   +   1   +   1   +   0
+// fib = 3
+// getFib(4) = 3
 
+// Iterative Fibonacci Number
+func iFibNum(_ position: Int) -> Int {
+    if position == 0 || position == 1 {
+        return position
+    }
+    
+    first = 0, second = 1
+    
+    for _ in 2...position {
+        fib = first + second
+        first = second
+        second = fib
+    }
+    
+    return fib
+}
+
+// This is how the iteration takes place
+// position = 4
+// getFib(4) = ?
+// (i = 2) fib = 0 + 1      ->     first = 1     ->     second = 1
+// (i = 3) fib = 1 + 1      ->     first = 1     ->     second = 2
+// (i = 4) fib = 1 + 2      ->     first = 2     ->     second = 3
+// fib = 3
+// getFib(4) = 3
