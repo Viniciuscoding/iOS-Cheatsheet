792ab16126f6f48e4c57436970b68daaeaf5a4b6
Viniciuscoding <vsg232@nyu.edu>
2020-04-27 23:08:14 -0400

    Update Strings.swift

--- Strings.swift
+++ Strings.swift
@@ -86,6 +86,35 @@ students.remove(at: 0) //["Ricardo", "Ryan", "Maria", "Elana", "Tax", "First", "
 // Removing the last elements from an array -> removeLast().
 students.removeLast() //["Ricardo", "Ryan", "Maria", "Elana", "Tax", "First"]
 
+// SUFIX and PREFIX
+let romeoAndJuliet = [
+    "Act 1 Scene 1: Verona, A public place",
+    "Act 1 Scene 2: Capulet's mansion",
+    "Act 1 Scene 3: A room in Capulet's mansion",
+    "Act 1 Scene 4: A street outside Capulet's mansion",
+    "Act 1 Scene 5: The Great Hall in Capulet's mansion",
+    "Act 2 Scene 1: Outside Capulet's mansion",
+    "Act 2 Scene 2: Capulet's orchard",
+    "Act 2 Scene 3: Outside Friar Lawrence's cell",
+    "Act 2 Scene 4: A street in Verona",
+    "Act 2 Scene 5: Capulet's mansion",
+    "Act 2 Scene 6: Friar Lawrence's cell"
+]
+
+// Prefix
+var act1SceneCount = 0
+for scene in romeoAndJuliet {
+  if scene.hasPrefix("Act 1 ") { act1SceneCount += 1 }
+}
+print("There are \(act1SceneCount) scenes in Act 1")      // Prints "There are 5 scenes in Act 1"
+
+// Sufix
+var mansionCount = 0, cellCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasSuffix("Capulet's mansion") { mansionCount += 1
+    } else if scene.hasSuffix("Friar Lawrence's cell") { cellCount += 1 }
+}
+print("\(mansionCount) mansion scenes; \(cellCount) cell scenes") // Prints "6 mansion scenes; 2 cell scenes"
 
 
 
